7e1f3704dd652e4b0cdc8d0b4a74057a4f2e0130
fadelafuente <f.delafuente@outlook.com>
2023-09-28 14:43:10 -0500

    add logger statements

--- src/main/java/io/getarrays/userservice/service/UserServiceImplementation.java
+++ src/main/java/io/getarrays/userservice/service/UserServiceImplementation.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static java.rmi.server.LogStream.log;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -20,16 +22,19 @@ public class UserServiceImplementation implements UserService {
     private final RoleRepo roleRepo;
     @Override
     public User saveUser(User user) {
+        log.info("saving new user {} to the database", user.getName());
         return userRepo.save(user);
     }
 
     @Override
     public Role saveRole(Role role) {
+        log.info("saving new role {} to the database", role.getName());
         return roleRepo.save(role);
     }
 
     @Override
     public void addRoleToUser(String username, String roleName) {
+        log.info("adding role {} to user {}", roleName, username);
         User user = userRepo.findByUsername(username);
         Role role = roleRepo.findByName(roleName);
         user.getRoles().add(role);
@@ -37,11 +42,13 @@ public class UserServiceImplementation implements UserService {
 
     @Override
     public User getUser(String username) {
+        log.info("fetching user {}", username);
         return userRepo.findByUsername(username);
     }
 
     @Override
     public List<User> getUsers() {
+        log.info("fetching all users");
         return userRepo.findAll();
     }
 }
